7347872e6477235611e70b452f53fb7112c65518
Raphael Nestler <rnestler@hsr.ch>
2012-04-25 10:37:26 +0200

    added missing files

---
+++ Includes/wall.h
@@ -0,0 +1,15 @@
+#ifndef WALL_H
+#define WALL_H
+
+typedef struct Wall {
+	float x;
+	float y1, y2;
+} Wall;
+
+void wall_init(Wall *wall, int length);
+void wall_update(Wall *wall, int dtime);
+void wall_draw(Wall *wall);
+void wall_clear(Wall *wall);
+
+#endif // WALL_H
+
---
+++ Source/wall.c
@@ -0,0 +1,31 @@
+#include "wall.h"
+#include <stdlib.h>
+#include "displayNeu.h"
+
+void wall_init(Wall *wall, int length)
+{
+	wall->x=160;
+	wall->y1=rand()%80;
+	wall->y2=wall->y1+length;
+}
+void wall_update(Wall *wall, int dtime)
+{
+	wall->x-=1;
+
+	if(wall->x<=0) {
+		wall->x=160;
+		int length = wall->y2-wall->y1+1;
+		wall->y1=rand()%80;
+		wall->y2=wall->y1+length;
+	}
+}
+
+void wall_draw(Wall *wall)
+{
+	DisplayDrawLine(wall->x,wall->y1,wall->x,wall->y2);
+}
+
+void wall_clear(Wall *wall)
+{
+	DisplayClrLine(wall->x,wall->y1,wall->x,wall->y2);
+}
