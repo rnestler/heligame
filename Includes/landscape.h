b7694196f6ca13ef0393811394a6d7acb0be3d5c
Raphael Nestler <rnestler@hsr.ch>
2012-04-25 11:21:45 +0200

    addad landscape

---
+++ Includes/landscape.h
@@ -0,0 +1,66 @@
+/********************************************************************************************
+  
+  File        : landscape.h
+  Author      : 
+  Date        : 
+  Last Update : 
+  Target      : MEGA332, BDM and CodeWarrior
+  
+  Beschreibung: Generiert zwei ungefähr parallel zueinander verlaufende Linienzüge, 
+                zwischen denen der Heli fliegen muss.
+********************************************************************************************/
+
+#ifndef LANDSCAPE_H
+#define LANDSCAPE_H 
+
+typedef struct {
+                 int xPnts[30];
+                 int yBottom[30];
+                 int yRoof[30];
+                    
+                 int yBorderLeftBottom;
+                 int yBorderRightBottom;
+                 int yBorderLeftRoof;
+                 int yBorderRightRoof;
+                    
+                 int nrOfPnts;
+                } Landscape;
+				
+
+/* Minimalabstand zw. zwei Eckpunten in x-Richtung */
+static const int minXDistance = 9; 
+
+/* zusätzlicher Abstand in x-Richtung, zufällig zw. 0 und angegebenem Wert */
+static const int rangeAdditionalXDistance = 20; 
+
+/* Höhe der unteren Eckpunkte in y-Richtung, 
+   zufällig zw. 0 und angegebenem Wert */
+static const int rangeYHight = 30; 
+
+/* zusätzlicher Abstand (zu deltaY) zwischen oberen und unteren Eckpunkten, 
+   in y-Richtung, zufällig zw. 0 und angegebenem Wert */
+static const int rangeAdditionalYDistance = 5; 
+
+/* rechter Displayrand */
+static const int xMax = 159; 
+
+/**
+* Initialisiert und zeichnet die Linienzüge über die Display-Breite 
+* mit zufällig angeordneten Bergspitzen und Talsohlen.
+* deltaY: Mindestabstand zwischen unterem und oberem Linienzug.
+*/
+void initLandscape(Landscape *lndscp, int deltaY);      
+
+/**
+* Lässt die Linienzüge um ein Pixel nach links wandern
+*/                            
+void landscapeFlow(Landscape *lndscp, int deltaY);    
+
+/**
+* gibt an, ob ein Punkt (pointX, pointY) den Linienzug (oben oder unten) berührt 
+* (oder bereits überschritten hat). 
+* Returnwert: 1 = berührt, 0 = berührt nicht
+*/
+int touch (Landscape *lndscp, int pointX, int pointY);
+
+#endif //LANDSCAPE_H
---
+++ Source/landscape.c
@@ -0,0 +1,211 @@
+/********************************************************************************************
+  
+
+  File        : landscape.c
+  Author      : 
+  Date        : 
+  Last Update : 
+  Target      : MEGA332, BDM and CodeWarrior
+  
+********************************************************************************************/
+#include "target.h"
+#include "displayNeu.h"
+#include <stdlib.h> /* für Zufallsgenerator rand() */
+
+#include "landscape.h" 
+
+ 
+void initLandscape(Landscape *lndscp, int deltaY)
+{
+  int j;
+
+  lndscp->nrOfPnts = 0;
+  lndscp->xPnts[0] = 0; 
+  
+  /*erster unterer Eckpunkt am linken Display-Rand */
+  lndscp->yBottom[0] = rand()%rangeYHight; 
+  
+  /*erster oberer Eckpunkt am linken Display-Rand */
+  lndscp->yRoof[0] = lndscp->yBottom[0] + deltaY + rand()%rangeAdditionalYDistance; 
+  
+  lndscp->yBorderLeftBottom = lndscp->yBottom[0];
+  lndscp->yBorderLeftRoof = lndscp->yRoof[0];
+  
+  /*weitere Eckpunkte ...*/
+  do 
+  {
+    lndscp->nrOfPnts++; 
+    lndscp->xPnts[lndscp->nrOfPnts] = lndscp->xPnts[lndscp->nrOfPnts - 1]
+                                      + minXDistance + rand() % rangeAdditionalXDistance;
+    lndscp->yBottom[lndscp->nrOfPnts] = rand() % rangeYHight;
+    lndscp->yRoof[lndscp->nrOfPnts] = lndscp->yBottom[lndscp->nrOfPnts] 
+                                      + deltaY + rand() % rangeAdditionalYDistance;
+  } while (lndscp->xPnts[lndscp->nrOfPnts] <= xMax); 
+  /* ... bis rechter Display-Rand überschritten */
+  
+  /* Eckpunkte zu Bergzügen verbinden (zeichnen) */
+  for (j = 1; j < lndscp->nrOfPnts; j++) 
+  {
+    DisplayDrawLine(lndscp->xPnts[j - 1], lndscp->yBottom[j - 1], 
+                    lndscp->xPnts[j], lndscp->yBottom[j]);
+    DisplayDrawLine(lndscp->xPnts[j-1], lndscp->yRoof[j - 1], 
+                    lndscp->xPnts[j], lndscp->yRoof[j]);
+  }
+  
+  /*Schnittpunkt des unteren Linienzugs mit rechtem Displayrand */
+  lndscp->yBorderRightBottom = lndscp->yBottom[lndscp->nrOfPnts - 1] 
+    + ((lndscp->yBottom[lndscp->nrOfPnts] - lndscp->yBottom[lndscp->nrOfPnts - 1])
+       * (xMax - lndscp->xPnts[lndscp->nrOfPnts - 1]))
+    / (lndscp->xPnts[lndscp->nrOfPnts] - lndscp->xPnts[lndscp->nrOfPnts - 1]);
+  
+  /*Schnittpunkt des oberen Linienzugs mit rechtem Displayrand */
+  lndscp->yBorderRightRoof = lndscp->yRoof[lndscp->nrOfPnts - 1] 
+    + ((lndscp->yRoof[lndscp->nrOfPnts] - lndscp->yRoof[lndscp->nrOfPnts - 1])
+       * (xMax - lndscp->xPnts[lndscp->nrOfPnts - 1]))
+    / (lndscp->xPnts[lndscp->nrOfPnts] - lndscp->xPnts[lndscp->nrOfPnts - 1]);
+  
+  /*letztes unteres Teilstück am rechten Rand zeichnen */
+  DisplayDrawLine(lndscp->xPnts[lndscp->nrOfPnts - 1], lndscp->yBottom[lndscp->nrOfPnts - 1], 
+                  xMax, lndscp->yBorderRightBottom); 
+  
+  /*letztes oberes Teilstück am rechten Rand zeichnen */
+  DisplayDrawLine(lndscp->xPnts[lndscp->nrOfPnts - 1], lndscp->yRoof[lndscp->nrOfPnts - 1], 
+                  xMax, lndscp->yBorderRightRoof); 
+}
+
+void landscapeFlow(Landscape *lndscp, int deltaY)
+{
+  int j;
+  
+  /*erstes unteres Teilstück am linken Rand löschen */
+  DisplayClrLine(0, lndscp->yBorderLeftBottom, 
+                 lndscp->xPnts[1], lndscp->yBottom[1]); 
+  
+  /*erstes oberes Teilstück am linken Rand löschen */  
+  DisplayClrLine(0, lndscp->yBorderLeftRoof, 
+                 lndscp->xPnts[1], lndscp->yRoof[1]); 
+  
+  /*alten Linienzug in der Mitte löschen */
+  for (j = 2; j < lndscp->nrOfPnts; j++) 
+  {
+    DisplayClrLine(lndscp->xPnts[j - 1], lndscp->yBottom[j - 1], 
+                   lndscp->xPnts[j], lndscp->yBottom[j]);
+    DisplayClrLine(lndscp->xPnts[j - 1], lndscp->yRoof[j - 1], 
+                   lndscp->xPnts[j], lndscp->yRoof[j]);
+  }
+ 
+  
+  /*letztes unteres Teilstück am rechten Rand löschen */
+  DisplayClrLine(lndscp->xPnts[lndscp->nrOfPnts - 1], lndscp->yBottom[lndscp->nrOfPnts - 1], 
+                 xMax, lndscp->yBorderRightBottom); 
+  
+  /*letztes oberes Teilstück am rechten Rand löschen */
+  DisplayClrLine(lndscp->xPnts[lndscp->nrOfPnts - 1], lndscp->yRoof[lndscp->nrOfPnts - 1], 
+                 xMax, lndscp->yBorderRightRoof); 
+  
+  /* alle Eckpunkte ein Pixel nach links schieben */
+  for (j = 0; j <= lndscp->nrOfPnts; j++) 
+  {
+    lndscp->xPnts[j]--;
+  }
+  
+  /* wenn hinterster Eckpunkt in Rahmen tritt --> neuer, zusätzlicher Punkt am rechten Ende des Linienzugs */
+  if (lndscp->xPnts[lndscp->nrOfPnts] < 160) 
+  {
+    lndscp->nrOfPnts++;
+    lndscp->xPnts[lndscp->nrOfPnts] = lndscp->xPnts[lndscp->nrOfPnts - 1]
+                                              + minXDistance + rand() % rangeAdditionalXDistance;
+    lndscp->yBottom[lndscp->nrOfPnts] = rand() % rangeYHight;
+    lndscp->yRoof[lndscp->nrOfPnts] = lndscp->yBottom[lndscp->nrOfPnts] 
+                                              + deltaY + rand() % rangeAdditionalYDistance;
+  }
+  
+  /* wenn zweitvorderster Eckpunkt am linken Rand aus Rahmen tritt --> Rochade und ein Punkt weniger */
+  if (lndscp->xPnts[1] < 0) 
+  {
+    for(j = 0; j < lndscp->nrOfPnts ; j++)
+    {
+      lndscp->xPnts[j] = lndscp->xPnts[j + 1];
+      lndscp->yBottom[j] = lndscp->yBottom[j + 1];
+      lndscp->yRoof[j] = lndscp->yRoof[j + 1];
+    }
+    lndscp->nrOfPnts--;
+  }
+  
+  /* neuer Schnittpunkt des unteren Linienzugs mit linkem Displayrand */
+  lndscp->yBorderLeftBottom = lndscp->yBottom[0] 
+    + ((lndscp->yBottom[1] - lndscp->yBottom[0])
+       * (0 - lndscp->xPnts[0]))
+    / (lndscp->xPnts[1] - lndscp->xPnts[0]);
+  
+  /* neuer Schnittpunkt des oberen Linienzugs mit linkem Displayrand */
+  lndscp->yBorderLeftRoof = lndscp->yRoof[0] 
+    + ((lndscp->yRoof[1]-lndscp->yRoof[0])
+       * (0 - lndscp->xPnts[0]))
+    / (lndscp->xPnts[1] - lndscp->xPnts[0]);
+  
+  /*erstes unteres Teilstück am linken Rand zeichnen */
+  DisplayDrawLine(0, lndscp->yBorderLeftBottom, 
+                  lndscp->xPnts[1], lndscp->yBottom[1]); 
+  
+  /*erstes oberes Teilstück am linken Rand zeichnen */  
+  DisplayDrawLine(0, lndscp->yBorderLeftRoof, 
+                  lndscp->xPnts[1], lndscp->yRoof[1]); 
+  
+  /* neuen Linienzug in der Mitte zeichnen */
+  for (j = 2; j < lndscp->nrOfPnts; j++) 
+  {
+    DisplayDrawLine(lndscp->xPnts[j - 1],  lndscp->yBottom[j - 1], 
+                    lndscp->xPnts[j], lndscp->yBottom[j]);
+    DisplayDrawLine(lndscp->xPnts[j - 1],  lndscp->yRoof[j - 1], 
+                    lndscp->xPnts[j], lndscp->yRoof[j]);
+  }
+  
+  /* neuer Schnittpunkt des unteren Linienzugs mit rechtem Displayrand */
+  lndscp->yBorderRightBottom = lndscp->yBottom[lndscp->nrOfPnts - 1] 
+    + ((lndscp->yBottom[lndscp->nrOfPnts] - lndscp->yBottom[lndscp->nrOfPnts - 1])
+       * (xMax - lndscp->xPnts[lndscp->nrOfPnts - 1]))
+    / (lndscp->xPnts[lndscp->nrOfPnts] - lndscp->xPnts[lndscp->nrOfPnts - 1]);
+  
+  /* neuer Schnittpunkt des oberen Linienzugs mit rechtem Displayrand */
+  lndscp->yBorderRightRoof = lndscp->yRoof[lndscp->nrOfPnts - 1] 
+    + ((lndscp->yRoof[lndscp->nrOfPnts] - lndscp->yRoof[lndscp->nrOfPnts - 1])
+       * (xMax - lndscp->xPnts[lndscp->nrOfPnts - 1]))
+    / (lndscp->xPnts[lndscp->nrOfPnts] - lndscp->xPnts[lndscp->nrOfPnts - 1]);
+  
+  /* letztes unteres Teilstück am rechten Rand zeichnen */
+  DisplayDrawLine(lndscp->xPnts[lndscp->nrOfPnts - 1], lndscp->yBottom[lndscp->nrOfPnts - 1], 
+                  xMax, lndscp->yBorderRightBottom); 
+  
+  /* letztes oberes Teilstück am rechten Rand */
+  DisplayDrawLine(lndscp->xPnts[lndscp->nrOfPnts - 1], lndscp->yRoof[lndscp->nrOfPnts - 1], 
+                  xMax, lndscp->yBorderRightRoof); 
+}
+
+
+int touch (Landscape *lndscp, int pointX, int pointY)
+{
+  int j = 0;
+  while (1)
+  {
+  j++;
+    if (pointX <= lndscp->xPnts[j])
+    {
+      if (
+           /* berührt oberen Linienzug NICHT */
+           ((pointX - lndscp->xPnts[j]) * (lndscp->yRoof[j - 1] - lndscp->yRoof[j]) 
+             - (pointY - lndscp->yRoof[j]) * (lndscp->xPnts[j - 1] - lndscp->xPnts[j]) 
+           < 0) 
+           &&
+           /* berührt unteren Linienzug NICHT */
+           ((pointX - lndscp->xPnts[j]) * (lndscp->yBottom[j - 1] - lndscp->yBottom[j])
+             - (pointY - lndscp->yBottom[j]) * (lndscp->xPnts[j - 1] - lndscp->xPnts[j]) 
+           > 0)
+         ) 
+        return 0;
+      else 
+        return 1;
+    }
+  } 
+}
+
